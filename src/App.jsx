90c19393ab16abc55251c244eb72f68c5b617767
roblesregina <regina.robles@anima.edu.uy>
2025-06-12 19:58:58 -0300

    Setup inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import movies from "./data/movies.json";
 
 function App() {
-  return <></>;
+  console.log(movies);
+  return <>Hola</>;
 }
 
 export default App;
